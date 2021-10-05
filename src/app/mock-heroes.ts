ce7e18fa73adb4b9f5fbb9e3b04ed4a44ee28c21
fradelycode <fradelydventura@gmail.com>
2021-10-05 09:56:08 -0400

    modify style reference

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Batman' },
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Wanda' },
+  { id: 14, name: 'Spiderman' },
+  { id: 15, name: 'Iron Man' },
+  { id: 16, name: 'Ant Man' },
+  { id: 17, name: 'Wonder Woman' },
+  { id: 18, name: 'Vision' },
+  { id: 19, name: 'Captain America' },
+  { id: 20, name: 'Hulk' },
+];
