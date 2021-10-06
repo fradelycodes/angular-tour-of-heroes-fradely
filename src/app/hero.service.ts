9ca441208aa9f2136077a47cb8beca176b2dcc99
fradelycode <fradelydventura@gmail.com>
2021-10-05 20:54:33 -0400

    final service, observable, dependecy injection

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
-  getHero(): Observable<Hero[]>{
+  getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
- }
-  constructor() { }
+  }
+  constructor(private messageService: MessageService) {}
 }
